5660dfbe2796e18fb1ca7c6efbe31bf817cb28fe
Melissa Vela <melissa@instarent.com.au>
2023-06-24 18:01:19 +1000

    products.json test

--- src/components/JobMatches.tsx
+++ src/components/JobMatches.tsx
@@ -2,13 +2,23 @@
 import {useParams} from 'react-router-dom'
 import { useEffect, useState } from 'react';
 import axios from 'axios'
-
+import {
+   TableContainer,
+   Table,
+   TableHead,
+   TableBody,
+   TableRow,
+   TableCell,
+   Paper,
+   Button,
+ } from "@mui/material";
+ import data from "./products.json";
 
 
 const JobMatches = () =>{
 
     const {id} = useParams();
-    const [matches, setMatches] = useState({});
+    const [matches, setMatches] = useState({} );
 
     useEffect(() => {
         axios(`https://test.swipejobs.com/api/worker/7f90df6e-b832-44e2-b624-3143d428001f/matches`).then(({ data }) => {
@@ -21,19 +31,23 @@ const JobMatches = () =>{
         return setMatches({});
      }, []);
     
-console.log(matches);
+console.log( matches);
 
     return (
         <>
         
         <div>
-      {matches.map((object, index) => (
-        <div key={index}>
-          <span>{object.jobId}</span>
-          <span>{object.jobTitle}</span>
-          {/* Add more properties as needed */}
-        </div>
-      ))}
+         
+
+      {
+         matches.map((match)=> (
+            <div>
+          
+            <TableCell>{match.jobId}</TableCell>
+            </div>
+             ))
+            
+      }
     </div>
  
 
